fd3ffda8f3c409968b90123c0e49e743c85b74f5
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2022-05-25 21:26:44 +0300

    Lesson 1: Task 1: Home work Test

---
+++ src/main/java/lv/javaguru/student_alexander_ivanov/lesson_1/task_1/HelloWorld.java
@@ -0,0 +1,8 @@
+package main.java.lv.javaguru.student_alexander_ivanov.lesson_1.task_1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
