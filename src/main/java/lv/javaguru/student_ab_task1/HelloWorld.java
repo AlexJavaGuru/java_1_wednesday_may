7bac548cd19c340737679a44c0275169e14f87cc
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-05-25 22:05:09 +0300

    ArtjomsB:task1

---
+++ src/main/java/lv/javaguru/student_ab_task1/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java.lv.javaguru.student_ab_task1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        var a= "ab";
+        if (a == "ab") {
+            System.out.println("Hello world");
+        }
+        else System.out.println("Goodbye world");
+    }
+}
