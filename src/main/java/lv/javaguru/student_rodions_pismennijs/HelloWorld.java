12bda4caf25c984157d36bf386a08d9039fa5b00
Somalo99 <rpismenny94@gmail.com>
2022-05-25 21:56:11 +0300

    test

---
+++ src/main/java/lv/javaguru/student_rodions_pismennijs/HelloWorld.java
@@ -0,0 +1,9 @@
+package main.java.lv.javaguru.student_rodions_pismennijs;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+
+}
