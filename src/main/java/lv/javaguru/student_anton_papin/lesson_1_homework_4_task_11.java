ebd7b9a9ed1c333c8d7e8829395c65820a44aa01
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-05-26 10:56:03 +0300

    lesson_1_homework_4_task_11

---
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_1_homework_4_task_11.java
@@ -0,0 +1,15 @@
+package main.java.lv.javaguru.student_anton_papin;
+
+public class lesson_1_homework_4_task_11 {
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        int c = 50;
+        int d = 3;
+
+        System.out.println (a + b);
+        System.out.println (c / d);
+
+
+    }
+}
