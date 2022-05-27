5a62e93ec56726f9f4b5d9bf8e816aa65bba8783
Maksims Sokolovs <sokolovsmaksims09@gmail.com>
2022-05-27 21:37:34 +0300

    HelloWorld Sokol

--- src/main/java/lv/javaguru/student_Maksims_Sokolovs/HelloWorld.java
+++ src/main/java/lv/javaguru/student_Maksims_Sokolovs/HelloWorld.java
@@ -1,8 +1,7 @@
 package main.java.lv.javaguru.student_Maksims_Sokolovs;
 
-import java.util.Scanner;
-
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello World from Maksims!!!");
     }
