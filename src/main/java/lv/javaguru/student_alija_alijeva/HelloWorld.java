56402a8effbd0a4bf0cbe768895fda2305ccb400
Alija <aliaalieva441@gmail.com>
2022-05-27 21:51:46 +0300

    HelloWorld

--- src/main/java/lv/javaguru/student_alija_alijeva/HelloWorld.java
+++ src/main/java/lv/javaguru/student_alija_alijeva/HelloWorld.java
@@ -2,5 +2,5 @@ package main.java.lv.javaguru.student_alija_alijeva;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World ");
 }
