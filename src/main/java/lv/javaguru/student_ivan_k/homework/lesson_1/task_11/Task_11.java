383e331214c4efcb7cd20dc99cc46b4e95ed20b0
ivan <Ivan.kornev@yahoo.com>
2022-05-27 17:35:05 +0300

    home_work_1

---
+++ src/main/java/lv/javaguru/student_ivan_k/homework/lesson_1/task_11/Task_11.java
@@ -0,0 +1,35 @@
+package main.java.lv.javaguru.student_ivan_k.homework.lesson_1.task_11;
+
+public class Task_11 {
+//    public static void main(String[] args) {
+//
+//        int a, b, c;
+//        a = 74;
+//        b = 36;
+//        c = a + b;
+//        System.out.println("  C is  sum a + b  = " +   c);
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        int a,b,c;
+
+        a = 50;
+        b = 3;
+        c = a/b;
+
+        System.out.println(" 50/3   =   "  +  c);
+
+
+    }
+
+
+
+}
