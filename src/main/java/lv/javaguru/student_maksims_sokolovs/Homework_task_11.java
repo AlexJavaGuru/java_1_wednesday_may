0d815edaa34d3fb3633f25f152847ebfaf638ffd
Maksims Sokolovs <sokolovsmaksims09@gmail.com>
2022-05-28 01:08:12 +0300

    Homework_task_11

--- src/main/java/lv/javaguru/student_maksims_sokolovs/Homework_task_11.java
+++ src/main/java/lv/javaguru/student_maksims_sokolovs/Homework_task_11.java
@@ -7,6 +7,7 @@ public class Homework_task_11 {
         int a = 74;
         int b = 36;
         System.out.println(a + b);
+
         int c = 50;
         int d = 3;
         System.out.println(c / d );
