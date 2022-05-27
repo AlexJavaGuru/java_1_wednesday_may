20dbf5ad6a5428d6f593dec4fec8807439ab56bc
Maksims Sokolovs <sokolovsmaksims09@gmail.com>
2022-05-27 21:34:46 +0300

    HelloWorld Sokol

--- src/main/java/lv/javaguru/student_Maksims_Sokolovs/HelloWorld.java
+++ src/main/java/lv/javaguru/student_Maksims_Sokolovs/HelloWorld.java
@@ -3,15 +3,8 @@ package main.java.lv.javaguru.student_Maksims_Sokolovs;
 import java.util.Scanner;
 
 public class HelloWorld {
-
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        int first, second, result;
-        System.out.print("Enter first num: ");
-        first = num.nextInt();
-        System.out.print("Enter second num: ");
-        second = num.nextInt();
-        result = first + second;
-        System.out.println("Result is - " + result);
+        System.out.println("Hello World from Maksims!!!");
     }
+
 }
