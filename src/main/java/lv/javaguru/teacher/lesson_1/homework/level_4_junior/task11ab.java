98a5589c3707c78f27548958b2bbb36d2d0bad30
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-05-28 16:21:54 +0300

    ArtjomsB:homework lecture 2,sorry for not sorting

---
+++ src/main/java/lv/javaguru/teacher/lesson_1/homework/level_4_junior/task11ab.java
@@ -0,0 +1,10 @@
+package main.java.lv.javaguru.teacher.lesson_1.homework.level_4_junior;
+
+public class task11ab {
+    public static void main(String[] args) {
+        var a=74+36;
+        var b=50/3;
+        System.out.println("74+36=" + a);
+        System.out.println("50/3=" + b);
+    }
+}
---
+++ src/main/java/lv/javaguru/teacher/lesson_2_variables/homework/level_6_middle/Task24AB.java
@@ -0,0 +1,8 @@
+package main.java.lv.javaguru.teacher.lesson_2_variables.homework.level_6_middle;
+
+public class Task24AB {
+    public static void main(String[] args) {
+    System.out.println("I wanna coffee");
+    System.out.println("So bad");
+    }
+}
