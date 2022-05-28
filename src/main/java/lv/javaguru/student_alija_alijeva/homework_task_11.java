e633bda9a04a9089861f1d526127067209afa950
Alijalijeva <aliaalieva441@gmail.com>
2022-05-28 12:44:37 +0300

    AlijaA_homework_task_11

--- src/main/java/lv/javaguru/student_alija_alijeva/homework_task_11.java
+++ src/main/java/lv/javaguru/student_alija_alijeva/homework_task_11.java
@@ -3,11 +3,18 @@ package main.java.lv.javaguru.student_alija_alijeva;
 public class homework_task_11 {
 
     public static void main(String[] args) {
-        int x = 74;
-        int y = 36;
-        int z = 50;
-        int w = 3;
-        System.out.println(x+y);
-        System.out.println(z/w);
+        int x, y, z;
+        x = 74;
+        y = 36;
+        z = x+y;
+        System.out.println(z);
+
+
+        int a, b, c;
+        a = 50;
+        b = 3;
+        c = a / b;
+        System.out.println(c);
+
     }
 }
