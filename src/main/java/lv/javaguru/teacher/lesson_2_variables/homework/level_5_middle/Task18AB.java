66d198332965ed92cde8fc86e55434277f25c32c
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-05-28 16:29:51 +0300

    Revert "ArtjomsB:homework lecture 2,sorry for not sorting"
    This reverts commit 98a5589c3707c78f27548958b2bbb36d2d0bad30.

--- src/main/java/lv/javaguru/teacher/lesson_2_variables/homework/level_5_middle/Task18AB.java
+++
@@ -1,8 +0,0 @@
-package main.java.lv.javaguru.teacher.lesson_2_variables.homework.level_5_middle;
-
-public class Task18AB {
-        public static void main(String[] args) {
-            System.out.println("Sorry, I have an errors.");
-            System.out.println("Please fix me!");
-        }
-    }
