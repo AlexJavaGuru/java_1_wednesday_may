385b7c245cd73f2eebefb05030b54323389477ab
Alijalijeva <aliaalieva441@gmail.com>
2022-06-03 01:23:45 +0300

    Lesson 2: Task 18

---
+++ src/main/java/lv/javaguru/student_alija_alijeva/lesson2/_Task_18/Task_18.java
@@ -0,0 +1,10 @@
+package main.java.lv.javaguru.student_alija_alijeva.lesson2._Task_18;
+
+
+    class SizeMatters {
+        public static void main(String[] args) {
+
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+    }
