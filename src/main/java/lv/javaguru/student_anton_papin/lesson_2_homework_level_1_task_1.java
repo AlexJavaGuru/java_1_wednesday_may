c081102cbd9a103385124b9de4ddb16f56e9406d
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-06-02 13:24:28 +0300

    lesson_1_homework_4_task_11

---
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework_level_1_task_1.java
@@ -0,0 +1,17 @@
+package main.java.lv.javaguru.student_anton_papin;
+
+import java.util.Scanner;
+
+public class lesson_2_homework_level_1_task_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+        int numberA = scanner.nextInt();
+        int numberB = scanner.nextInt();
+        System.out.println( numberA + numberB );
+        System.out.println( numberA - numberB );
+        System.out.println( numberA * numberB );
+        System.out.println( numberA / numberB );
+
+    }
+}
