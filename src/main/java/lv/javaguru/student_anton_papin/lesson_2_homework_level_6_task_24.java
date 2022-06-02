600269abe4d7cdbc8c76b5d052e65b89f46e3d2c
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-06-02 15:22:22 +0300

    task 24

---
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework_level_6_task_24.java
@@ -0,0 +1,8 @@
+package main.java.lv.javaguru.student_anton_papin;
+
+public class lesson_2_homework_level_6_task_24 {public static void main(String[] args) {
+    System.out.println("I wanna coffee");
+    System.out.println("So bad");
+}
+}
+
