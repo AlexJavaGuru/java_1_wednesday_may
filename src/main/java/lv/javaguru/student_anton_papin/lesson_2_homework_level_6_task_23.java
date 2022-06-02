77e216e1201564405fd78e1831fd2c8b9c738b37
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-06-02 15:21:10 +0300

    task 23

---
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework_level_6_task_23.java
@@ -0,0 +1,8 @@
+package main.java.lv.javaguru.student_anton_papin;
+
+public class lesson_2_homework_level_6_task_23 {
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
