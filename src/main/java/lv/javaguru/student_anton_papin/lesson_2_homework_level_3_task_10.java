53e7ffb36fe4bc82b10f2bb481018e13762b56e9
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-06-02 14:34:31 +0300

    task 10

---
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework_level_3_task_10.java
@@ -0,0 +1,12 @@
+package main.java.lv.javaguru.student_anton_papin;
+
+import java.util.Scanner;
+
+public class lesson_2_homework_level_3_task_10 {public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(" Please, enter the circle radius.");
+    double radius = scanner.nextDouble();
+    System.out.println("Perimeter" + "="+ 2 * radius * 3.141592653589793);
+    System.out.println("Area" + "=" + radius * radius *3.141592653589793);
+}
+}
