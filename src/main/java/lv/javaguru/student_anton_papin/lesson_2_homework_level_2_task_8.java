9c7125b19e6959d8e3750db88fb4350f748f0263
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-06-02 13:52:09 +0300

    task 8

---
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework_level_2_task_8.java
@@ -0,0 +1,12 @@
+package main.java.lv.javaguru.student_anton_papin;
+
+import java.util.Scanner;
+
+public class lesson_2_homework_level_2_task_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter your name, please.");
+        String nameA = scanner.next();
+        System.out.println("Hello " + nameA);
+    }
+}
