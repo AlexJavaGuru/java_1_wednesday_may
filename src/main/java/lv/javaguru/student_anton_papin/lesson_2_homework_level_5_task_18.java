1dc8815e7e2c583d5f31c2b5461ce09a90232407
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-06-02 15:10:29 +0300

    task 18

---
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework_level_5_task_18.java
@@ -0,0 +1,11 @@
+package main.java.lv.javaguru.student_anton_papin;
+
+public class lesson_2_homework_level_5_task_18 {public static void main(String[] args) {
+    System.out.println("Sorry, I have an errors.");
+    System.out.println("Please fix me!");
+    System.out.println("***bug fixing in process***");
+    System.out.println("Oh, now i am feeling well!");
+    System.out.println("Thank you!");
+}
+}
+
