b5b1d701ca294bdfb4c5502a05659caa296dd038
Alijalijeva <aliaalieva441@gmail.com>
2022-06-03 14:37:11 +0300

    Lesson 2: Task 7

--- src/main/java/lv/javaguru/student_alija_alijeva/lesson2/homework/level_2/task_7/TaskSeven.java
+++ src/main/java/lv/javaguru/student_alija_alijeva/lesson2/homework/level_2/task_7/TaskSeven.java
@@ -5,7 +5,7 @@ package main.java.lv.javaguru.student_alija_alijeva.lesson2.homework.level_2.tas
 
         public static void main(String[] args) {
             System.out.println("You can login!");
-            
+
         }
 
 
