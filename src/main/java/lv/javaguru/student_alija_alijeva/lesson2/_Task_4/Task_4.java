6b127e064b5d48954b6e41f0ca594afa3db0de7f
Alijalijeva <aliaalieva441@gmail.com>
2022-06-03 14:35:29 +0300

    Lesson 2: Task 18

--- src/main/java/lv/javaguru/student_alija_alijeva/lesson2/_Task_4/Task_4.java
+++
@@ -1,7 +0,0 @@
-package main.java.lv.javaguru.student_alija_alijeva.lesson2._Task_4;
-
-
-    class Phone{
-
-}
-
---
+++ src/main/java/lv/javaguru/student_alija_alijeva/lesson2/homework/level_2/task_7/TaskSeven.java
@@ -0,0 +1,14 @@
+package main.java.lv.javaguru.student_alija_alijeva.lesson2.homework.level_2.task_7;
+
+
+    class LoginService {
+
+        public static void main(String[] args) {
+            System.out.println("You can login!");
+            
+        }
+
+
+    }
+
+
