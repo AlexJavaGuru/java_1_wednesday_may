5bda3f9ca611129de0987e7ec0e77473ce01bf05
Maksims Sokolovs <sokolovsmaksims09@gmail.com>
2022-06-04 22:08:44 +0300

    Sokolovs_M_lesson_2_level_1_task_1

---
+++ src/main/java/lv/javaguru/student_maksims_sokolovs/level_1_task_1.java
@@ -0,0 +1,25 @@
+package main.java.lv.javaguru.student_maksims_sokolovs;
+
+import java.util.Scanner;
+
+public class level_1_task_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number ");
+        int a = scanner.nextInt();
+        System.out.println("Enter the second number ");
+        int b = scanner.nextInt();
+
+        int add = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+
+        System.out.println("The result of addition = " + (a + b));
+        System.out.println("The result of subtraction = " + (a - b));
+        System.out.println("The result of multiplication = " + (a * b));
+        System.out.println("Division result = " + ((double) a / b));
+    }
+}
