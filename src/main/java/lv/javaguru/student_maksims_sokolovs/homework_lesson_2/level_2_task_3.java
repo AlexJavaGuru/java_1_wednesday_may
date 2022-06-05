b8507cb1066d6264b3dfec6d5ccc5fa2334940a9
Maksims Sokolovs <sokolovsmaksims09@gmail.com>
2022-06-05 18:54:07 +0300

    sokolovs_m_lesson_2

--- src/main/java/lv/javaguru/student_maksims_sokolovs/homework_lesson_2/level_2_task_3.java
+++ src/main/java/lv/javaguru/student_maksims_sokolovs/homework_lesson_2/level_2_task_3.java
@@ -1,7 +1,5 @@
 package main.java.lv.javaguru.student_maksims_sokolovs.homework_lesson_2;
 
-    class Book {
+class Book {
 
-    }
-
-
+}
--- src/main/java/lv/javaguru/student_maksims_sokolovs/homework_lesson_2/level_2_task_4.java
+++ src/main/java/lv/javaguru/student_maksims_sokolovs/homework_lesson_2/level_2_task_4.java
@@ -1,6 +1,6 @@
 package main.java.lv.javaguru.student_maksims_sokolovs.homework_lesson_2;
 
-    class Phone {
+class Phone {
 
-    }
+}
 
