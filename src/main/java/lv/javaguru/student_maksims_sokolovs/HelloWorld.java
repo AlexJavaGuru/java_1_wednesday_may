a3182adab9469bb8f5b9db276e735101f8074718
Maksims Sokolovs <sokolovsmaksims09@gmail.com>
2022-06-05 21:07:29 +0300

    sokolovs_m_lesson_2

--- src/main/java/lv/javaguru/student_maksims_sokolovs/HelloWorld.java
+++ src/main/java/lv/javaguru/student_maksims_sokolovs/HelloWorld.java
@@ -1,18 +1,10 @@
 package main.java.lv.javaguru.student_maksims_sokolovs;
 
-import java.util.Scanner;
-
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Type your name...");
-
-        String name = scanner.nextLine();
-
-        System.out.println("Hello, " + name);
 
     }
 
