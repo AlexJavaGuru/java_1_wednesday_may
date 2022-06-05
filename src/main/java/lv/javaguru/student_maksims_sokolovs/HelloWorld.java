c1a2363db56bcfc7b4a52abacfa2187b9c458a2b
Maksims Sokolovs <sokolovsmaksims09@gmail.com>
2022-06-06 00:04:40 +0300

    sokolovs_m_lesson_2

--- src/main/java/lv/javaguru/student_maksims_sokolovs/HelloWorld.java
+++ src/main/java/lv/javaguru/student_maksims_sokolovs/HelloWorld.java
@@ -1,8 +1,23 @@
 package main.java.lv.javaguru.student_maksims_sokolovs;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type radius...");
+        double radius = scanner.nextDouble();
+        double perimeter = 2 * Math.PI * radius;
+        System.out.println("Perimeter is " + perimeter);
+        double area = Math.PI * radius * radius;
+        System.out.println("Area is " + area);
+
+
+
+
+
 
     }
 
---
+++ src/main/java/lv/javaguru/student_maksims_sokolovs/homework_lesson_2/level_3_task_10.java
@@ -0,0 +1,22 @@
+package main.java.lv.javaguru.student_maksims_sokolovs.homework_lesson_2;
+
+import java.util.Scanner;
+
+public class level_3_task_10 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter radius: 7,5");
+
+        double r = scanner.nextDouble();
+
+        double p = 2 * Math.PI * r;
+        System.out.println("Perimeter is = " + p);
+
+        double area = Math.PI * r * r;
+        System.out.println("Area is = " + area);
+
+    }
+
+}
