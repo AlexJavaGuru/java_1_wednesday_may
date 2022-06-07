6a2838f53ef5b5a4beee0f46eb2f5a11f1d441b0
Miroslavs Fjodorovs <106261828+PecnayHeP@users.noreply.github.com>
2022-06-07 20:58:52 +0300

    Lesson 2: Homework tasks 1-14

---
+++ src/main/java/lv/javaguru/student_miroslavs_fjodorovs/lesson_2/Task_1.java
@@ -0,0 +1,23 @@
+package main.java.lv.javaguru.student_miroslavs_fjodorovs.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("type number a...");
+        int firstNumber = scanner.nextInt();
+        System.out.println("type number b ...");
+        int secondNumber = scanner.nextInt();
+
+       System.out.println(firstNumber + secondNumber);
+       System.out.println(firstNumber - secondNumber);
+       System.out.println(firstNumber * secondNumber);
+       System.out.println(firstNumber / secondNumber);
+
+
+
+    }
+}
---
+++ src/main/java/lv/javaguru/student_miroslavs_fjodorovs/lesson_2/Task_10.java
@@ -0,0 +1,17 @@
+package main.java.lv.javaguru.student_miroslavs_fjodorovs.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_10 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Radius = ");
+        double radius = scanner.nextDouble();
+
+        System.out.println("Perimeter" + " " + "is" + " " + "=" + " " + 2 * 3.14 * radius);
+        System.out.println("Area" + " " + "is" + " " + "=" + " " + 3.14 * radius * radius);
+
+    }
+}
