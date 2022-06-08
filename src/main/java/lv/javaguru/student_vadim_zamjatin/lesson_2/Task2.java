1145e52d642fc5cb81f54905b60b61f2c2004dfb
vadzam <vadzam777@gmail.com>
2022-06-08 21:38:08 +0300

    Vadim Zamjatin lesson2: task 1-8,29,32

---
+++ src/main/java/lv/javaguru/student_vadim_zamjatin/lesson_2/Task2.java
@@ -0,0 +1,21 @@
+package main.java.lv.javaguru.student_vadim_zamjatin.lesson_2;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+        double firstDoubleNumber = scanner.nextDouble();
+        double secondDoubleNumber = scanner.nextDouble();
+        System.out.println();
+
+        System.out.println(firstDoubleNumber + secondDoubleNumber);
+        System.out.println(firstDoubleNumber - secondDoubleNumber);
+        System.out.println(firstDoubleNumber * secondDoubleNumber);
+        System.out.println(firstDoubleNumber / secondDoubleNumber);
+    }
+
+}
