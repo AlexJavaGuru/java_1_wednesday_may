c644f359026e56ca46356e631cbed0b2b17f8994
t3od0r <106261183+t3od0r@users.noreply.github.com>
2022-06-08 12:53:37 +0300

    Fixing format

--- src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework/level_6/NoSleepNoFocus.java
+++ src/main/java/lv/javaguru/student_anton_papin/lesson_2_homework/level_6/NoSleepNoFocus.java
@@ -1,8 +1,9 @@
 package main.java.lv.javaguru.student_anton_papin.lesson_2_homework.level_6;
 
-public class NoSleepNoFocus {public static void main(String[] args) {
-    System.out.println("I wanna coffee");
-    System.out.println("So bad");
-}
-}
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
 
+    }
+}
