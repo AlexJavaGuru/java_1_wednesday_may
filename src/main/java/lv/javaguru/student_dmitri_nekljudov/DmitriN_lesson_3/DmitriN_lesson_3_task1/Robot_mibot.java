799730042682bc7c0c058df0fbfa6aadbc63d601
Dmitri1302 <jogurt4420@gmail.com>
2022-06-09 23:20:15 +0300

    Dmitri N Lesson 3: Task: 1,2,3-6,7,8,9

---
+++ src/main/java/lv/javaguru/student_dmitri_nekljudov/DmitriN_lesson_3/DmitriN_lesson_3_task1/Robot_mibot.java
@@ -0,0 +1,14 @@
+package main.java.lv.javaguru.student_dmitri_nekljudov.DmitriN_lesson_3.DmitriN_lesson_3_task1;
+
+import main.java.lv.javaguru.student_dmitri_nekljudov.DmitriN_lesson_3.DmitriN_lesson_3_task1.Robot;
+
+public class Robot_mibot {
+
+        public static void main(String args[]){
+
+            Robot mibot = new Robot();
+
+            mibot.say();
+
+    }
+}
---
+++ src/main/java/lv/javaguru/student_dmitri_nekljudov/DmitriN_lesson_3/DmitriN_lesson_3_task8/HiName.java
@@ -0,0 +1,12 @@
+package main.java.lv.javaguru.student_dmitri_nekljudov.DmitriN_lesson_3.DmitriN_lesson_3_task8;
+
+public class HiName {
+
+    public static void main(String[] args) {
+
+        String myName = "Dmitri";
+        System.out.println("Hi " + " Dmitri " + "!" );
+
+    }
+}
+
