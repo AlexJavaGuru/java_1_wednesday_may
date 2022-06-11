8b4a463a2e66772322db2889d7ce35432c220b93
Wanxezar <zenadik@inbox.lv>
2022-06-11 15:27:06 +0300

    DD: HwLesson2

---
+++ src/main/java/lv/javaguru/student_daniels_demishins/lesson_3/lvl_1/Robot.java
@@ -0,0 +1,13 @@
+package main.java.lv.javaguru.student_daniels_demishins.lesson_3.lvl_1;
+
+public class Robot {
+    public Robot() {
+    }
+    public void sayHello(){
+        System.out.println("Hello....");
+    }
+    public void sayYourName(){
+        System.out.println("My name is Robo");
+    }
+}
+
---
+++ src/main/java/lv/javaguru/student_daniels_demishins/lesson_3/lvl_1/RobotDemo.java
@@ -0,0 +1,14 @@
+package main.java.lv.javaguru.student_daniels_demishins.lesson_3.lvl_1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot1= new Robot();
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2= new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+}
