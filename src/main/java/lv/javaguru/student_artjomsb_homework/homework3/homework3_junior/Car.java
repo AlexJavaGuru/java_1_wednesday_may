e3bdb6fad7583971b67990533bc1e16f0da511d4
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-06-11 23:55:10 +0300

    Lesson3 homework3: Finished + Car

---
+++ src/main/java/lv/javaguru/student_artjomsb_homework/homework3/homework3_junior/Car.java
@@ -0,0 +1,25 @@
+package main.java.lv.javaguru.student_artjomsb_homework.homework3.homework3_junior;
+
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
