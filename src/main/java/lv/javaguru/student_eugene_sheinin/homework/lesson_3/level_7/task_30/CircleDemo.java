4f1d5fd62a7d4c31e6a9069c4861bc976c9ad6e0
Eugene Sheinin <favor2684@gmail.com>
2022-06-13 01:59:29 +0300

    Lesson 3 homework from student Eugene Sheinin

---
+++ src/main/java/lv/javaguru/student_eugene_sheinin/homework/lesson_3/level_7/task_30/CircleDemo.java
@@ -0,0 +1,10 @@
+package main.java.lv.javaguru.student_eugene_sheinin.homework.lesson_3.level_7.task_30;
+
+public class CircleDemo {
+
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(3.45);
+        System.out.printf("Circle area %.2f", circle.calculateArea());
+    }
+}
