f9d9953859e23de34ae5d44dc37d880d8073cfbf
Wanxezar <zenadik@inbox.lv>
2022-06-12 21:16:55 +0300

    DD: HwLesson2-fixed

--- src/main/java/lv/javaguru/student_daniels_demishins/hw_lesson_2/lvl_3/TaskEleven.java
+++ src/main/java/lv/javaguru/student_daniels_demishins/hw_lesson_2/lvl_3/TaskEleven.java
@@ -6,9 +6,9 @@ public class TaskEleven {
     public static void main(String[] args) {
         System.out.println("Введите 3 целых числа");
         Scanner scanner=new Scanner(System.in);
-        int a= scanner.nextInt();
-        int b= scanner.nextInt();
-        int c= scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
         int result= a+b+c;
         System.out.println("Средние арифметическое = " +result /3 );
