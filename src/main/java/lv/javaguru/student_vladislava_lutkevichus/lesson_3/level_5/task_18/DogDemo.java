66895d0111bcb843429de5ba28997515aa0f7b19
Vladislava-L <72414681+Vladislava-L@users.noreply.github.com>
2022-06-13 00:22:36 +0300

    Lesson 3 Level_5 Tasks no.18,19,20 completed

---
+++ src/main/java/lv/javaguru/student_vladislava_lutkevichus/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,12 @@
+package main.java.lv.javaguru.student_vladislava_lutkevichus.lesson_3.level_5.task_18;
+
+public class DogDemo {
+
+    public static void main(String[] args){
+        Dog myDog = new Dog("Bobik");
+
+        System.out.println(myDog.getName());
+
+        myDog.voice();
+    }
+}
---
+++ src/main/java/lv/javaguru/student_vladislava_lutkevichus/lesson_3/level_5/task_20/DogDemo.java
@@ -0,0 +1,17 @@
+package main.java.lv.javaguru.student_vladislava_lutkevichus.lesson_3.level_5.task_20;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Bobik", 2);
+
+        System.out.println(myDog.age);
+
+        myDog.voice();
+        myDog.happyBirthday();
+
+        myDog.voice();
+        myDog.happyBirthday();
+
+        myDog.voice();
+    }
+}
