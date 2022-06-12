1cfea112839bc8a0919233a43dc7c091e1472277
Vadim_V <dogfather@inbox.lv>
2022-06-12 12:09:05 +0300

    Vadims_Viljhovojs_Lesson_2_2 task_11,18,23,24,29,30,32

---
+++ src/main/java/lv/javaguru/student_vadims_vilhovojs/lesson_2/task11.java
@@ -0,0 +1,20 @@
+package main.java.lv.javaguru.student_vadims_vilhovojs.lesson_2;
+
+import java.util.Scanner;
+
+public class task11 {
+    public static void main(String[] args) {
+        System.out.println("specify three integers");
+
+        Scanner scanner = new Scanner(System.in);
+        int variableA = scanner.nextInt();
+        int variableB = scanner.nextInt();
+        int variableC = scanner.nextInt();
+        double result = ((variableA + variableB + variableC) / 3) ;
+
+        System.out.println(result);
+
+
+
+    }
+}
---
+++ src/main/java/lv/javaguru/student_vadims_vilhovojs/lesson_2/task29.java
@@ -0,0 +1,17 @@
+package main.java.lv.javaguru.student_vadims_vilhovojs.lesson_2;
+
+public class task29 {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        System.out.println("Sum = " + sum);
+
+
+
+    }
+
+}
