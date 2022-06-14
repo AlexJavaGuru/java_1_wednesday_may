cfc53b0bb457fdec078d80cf3cb843d36ab1b477
Dmitri1302 <jogurt4420@gmail.com>
2022-06-14 22:51:42 +0300

    Dmitri N Lesson 2 popravka

--- src/main/java/lv/javaguru/student_dmitri_nekljudov/DmitriN_lesson_2/DmitriN_lesson_2_task_15/Text.java
+++ src/main/java/lv/javaguru/student_dmitri_nekljudov/DmitriN_lesson_2/DmitriN_lesson_2_task_15/Text.java
@@ -1,8 +1,9 @@
 package main.java.lv.javaguru.student_dmitri_nekljudov.DmitriN_lesson_2.DmitriN_lesson_2_task_15;
 
-public class Text {
+class Text {
     public static void main(String[] args) {
 
-         System.out.print("text");
+        System.out.print("text");
+
     }
 }
--- src/main/java/lv/javaguru/student_dmitri_nekljudov/DmitriN_lesson_2/DmitriN_lesson_2_task_18/SizeMatters.java
+++ src/main/java/lv/javaguru/student_dmitri_nekljudov/DmitriN_lesson_2/DmitriN_lesson_2_task_18/SizeMatters.java
@@ -1,8 +1,7 @@
 package main.java.lv.javaguru.student_dmitri_nekljudov.DmitriN_lesson_2.DmitriN_lesson_2_task_18;
 
-public class SizeMatters {
-        public static void main(String[] args) {
-
+class SizeMatters {
+    public static void main(String[] args) {
 
         System.out.println("Sorry, I have an errors.");
         System.out.println("Please fix me!");
