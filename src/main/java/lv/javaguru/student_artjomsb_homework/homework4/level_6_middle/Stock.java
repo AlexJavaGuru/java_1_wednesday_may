7cbda59160030f9fd92c8e5bad831adb9891f76f
ArtjomsB <ar.bogatirjovs@gmail.com>
2022-06-28 14:40:01 +0300

    Lesson 4: Stock finished

--- src/main/java/lv/javaguru/student_artjomsb_homework/homework4/level_6_middle/Stock.java
+++ src/main/java/lv/javaguru/student_artjomsb_homework/homework4/level_6_middle/Stock.java
@@ -2,18 +2,18 @@ package main.java.lv.javaguru.student_artjomsb_homework.homework4.level_6_middle
 
 class Stock {
     final private String companyName;
-    double currentPrice;
-    private double minPrice;
-    private double maxPrice;
+    int currentPrice;
+    private int minPrice;
+    private int maxPrice;
 
-    Stock(String companyName, double currentPrice) {
+    Stock(String companyName, int currentPrice) {
         this.companyName = companyName;
         this.currentPrice = currentPrice;
         this.minPrice = currentPrice;
         this.maxPrice = currentPrice;
     }
 
-    void setCurrentPrice(double newPrice) {
+    void setCurrentPrice(int newPrice) {
         this.currentPrice = newPrice;
         if (currentPrice > maxPrice) {
             this.maxPrice = currentPrice;
@@ -24,6 +24,6 @@ class Stock {
     }
 
     String getPriceInformation() {
-        return "Company = \"" + companyName + "\", Current Price = " + currentPrice + ", Min Price = " + minPrice + ", Max Price = " + maxPrice;
+        return "Company = \"" + companyName + "\" ,Current Price = " + currentPrice + " ,Min Price = " + minPrice + " ,Max Price = " + maxPrice;
     }
 }
--- src/main/java/lv/javaguru/student_artjomsb_homework/homework4/level_6_middle/StockTest.java
+++ src/main/java/lv/javaguru/student_artjomsb_homework/homework4/level_6_middle/StockTest.java
@@ -1,25 +1,72 @@
 package main.java.lv.javaguru.student_artjomsb_homework.homework4.level_6_middle;
 
- class StockTest {
+class StockTest {
 
-     public static void main(String[] args) {
-         Stock google = new Stock("Google", 10);
-         System.out.println(google.getPriceInformation());
-         google.setCurrentPrice(53);
-         google.setCurrentPrice(12);
-         google.setCurrentPrice(143);
-         google.setCurrentPrice(3);
-         google.setCurrentPrice(97);
-         System.out.println(google.getPriceInformation());
 
-         Stock amd = new Stock("AMD", 103);
-         System.out.println(amd.getPriceInformation());
-         amd.setCurrentPrice(98);
-         amd.setCurrentPrice(54);
-         amd.setCurrentPrice(109);
-         amd.setCurrentPrice(71);
-         amd.setCurrentPrice(102);
-         System.out.println(amd.getPriceInformation());
+    public static void main(String[] args) {
+        StockTest runner = new StockTest();
+        runner.testMaxPrice();
+        runner.testMinPrice();
+        runner.testCurrentPrice();
+    }
 
-     }
- }
+    void testCurrentPrice() {
+        Stock testCurrentStock = new Stock("Test", 5);
+        int expectedResult = 5;
+        int result = testCurrentStock.currentPrice;
+        check(result == expectedResult, "Test current Price");
+        testCurrentStock.setCurrentPrice(532);
+        expectedResult = 532;
+        result = testCurrentStock.currentPrice;
+        check(result == expectedResult, "Test current Price, number Two");
+    }
+
+    void testMaxPrice() {
+        Stock testMaxStock = new Stock("Test", 10);
+        int expectedResult = 999;
+        testMaxStock.setCurrentPrice(999);
+        int result = Integer.parseInt(testMaxStock.getPriceInformation().replaceAll(".*\s(?=.*$)", ""));
+        check(result == expectedResult, "Test max Price");
+        testMaxStock.setCurrentPrice(1);
+        result = Integer.parseInt(testMaxStock.getPriceInformation().replaceAll(".*\s(?=.*$)", ""));
+        check(result == expectedResult, "Test max Price,number Two");
+        testMaxStock.setCurrentPrice(1000);
+        expectedResult = 1000;
+        result = Integer.parseInt(testMaxStock.getPriceInformation().replaceAll(".*\s(?=.*$)", ""));
+        check(result == expectedResult, "Test max Price,number Three");
+        testMaxStock.setCurrentPrice(2);
+        testMaxStock.setCurrentPrice(999);
+        testMaxStock.setCurrentPrice(943);
+        result = Integer.parseInt(testMaxStock.getPriceInformation().replaceAll(".*\s(?=.*$)", ""));
+        check(result == expectedResult, "Test max Price,number Four");
+    }
+
+    void testMinPrice() {
+        Stock testMinStock = new Stock("Test", 10);
+        int expectedResult = 10;
+        testMinStock.setCurrentPrice(20);
+        String[] priceInformation = testMinStock.getPriceInformation().split(" ", 14);
+        int result = Integer.parseInt(priceInformation[10]);
+        check(result == expectedResult, "Test min Price");
+        testMinStock.setCurrentPrice(2);
+        testMinStock.setCurrentPrice(333);
+        expectedResult = 2;
+        priceInformation = testMinStock.getPriceInformation().split(" ", 14);
+        result = Integer.parseInt(priceInformation[10]);
+        check(result == expectedResult, "Test min Price,number Two");
+        testMinStock.setCurrentPrice(5032);
+        testMinStock.setCurrentPrice(23);
+        testMinStock.setCurrentPrice(1);
+        testMinStock.setCurrentPrice(243);
+        testMinStock.setCurrentPrice(4743);
+        expectedResult = 1;
+        priceInformation = testMinStock.getPriceInformation().split(" ", 14);
+        result = Integer.parseInt(priceInformation[10]);
+        check(result == expectedResult, "Test min Price,number Three");
+    }
+
+    void check(boolean isPassed, String testName) {
+        if (isPassed) System.out.println("Test - " + testName + " , Passed!");
+        else System.out.println("Test - " + testName + " , FAILED");
+    }
+}
