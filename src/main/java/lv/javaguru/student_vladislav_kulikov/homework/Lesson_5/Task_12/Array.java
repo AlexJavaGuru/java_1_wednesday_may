b5951bfac1815b912d37ac4d1c3e9db718dc8ca1
VladKuli <goldydefault@gmail.com>
2022-06-29 23:51:10 +0300

    Vladislav Kulikov Lesson 5: Task 1-24

---
+++ src/main/java/lv/javaguru/student_vladislav_kulikov/homework/Lesson_5/Task_12/Array.java
@@ -0,0 +1,20 @@
+package main.java.lv.javaguru.student_vladislav_kulikov.homework.Lesson_5.Task_12;
+
+import java.util.Random;
+
+class Array {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(10);
+        }
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
