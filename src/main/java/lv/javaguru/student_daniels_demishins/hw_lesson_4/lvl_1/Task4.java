dc1350ec687ddba76ee8c28a3b8220b1fe50a9c0
Wanxezar <zenadik@inbox.lv>
2022-06-29 17:39:03 +0300

    DD: HwLesson4

---
+++ src/main/java/lv/javaguru/student_daniels_demishins/hw_lesson_4/lvl_1/Task4.java
@@ -0,0 +1,16 @@
+package main.java.lv.javaguru.student_daniels_demishins.hw_lesson_4.lvl_1;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        int a = scanner.nextInt();
+
+        if (a (/2)){
+            System.out.println("");
+        }
+
+    }
+}
