054fda8689382d92867bf27dd9e778dc72aa82b6
ArtjomsB <ar.bogatirjovs@gmail.com>
2022-07-01 21:19:02 +0300

    Lesson 5: homework5 finished

---
+++ src/main/java/lv/javaguru/student_artjomsb_homework/homework5/level_5and6_middle/ArrayUtil.java
@@ -0,0 +1,60 @@
+package main.java.lv.javaguru.student_artjomsb_homework.homework5.level_5and6_middle;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class ArrayUtil {
+    Random random = new Random();
+
+    int[] createArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+
+    void fillArrayWithRandomNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+    }
+
+    void printArrayToConsole(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+
+    int findMaxNumber(int[] array) {
+        int maxNumber = array[0];
+        for (int number : array) {
+            if (maxNumber < number) maxNumber = number;
+        }
+        return maxNumber;
+    }
+
+    int findMinNumber(int[] array) {
+        int minNumber = array[0];
+        for (int number : array) {
+            if (minNumber > number) minNumber = number;
+        }
+        return minNumber;
+    }
+
+    int[] findOddNumbers(int[] array) {
+        int[] oddNumbers = new int[0];
+        for (int number : array) {
+            if (number % 2 != 0) {
+                oddNumbers = Arrays.copyOf(oddNumbers, oddNumbers.length + 1);
+                oddNumbers[oddNumbers.length - 1] = number;
+            }
+        }
+        return oddNumbers;
+    }
+
+    int[] findEvenNumbers(int[] array) {
+        int[] evenNumbers = new int[0];
+        for (int number : array) {
+            if (number % 2 == 0) {
+                evenNumbers = Arrays.copyOf(evenNumbers, evenNumbers.length + 1);
+                evenNumbers[evenNumbers.length - 1] = number;
+            }
+        }
+        return evenNumbers;
+    }
+}
