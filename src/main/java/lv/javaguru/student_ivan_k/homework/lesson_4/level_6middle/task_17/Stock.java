60570abea744477655d44bea346444b1dbadbc77
ivan <Ivan.kornev@yahoo.com>
2022-07-05 13:00:47 +0300

    home_work_Lesson_4 1-16

--- src/main/java/lv/javaguru/student_ivan_k/homework/lesson_4/level_6middle/task_17/Stock.java
+++ src/main/java/lv/javaguru/student_ivan_k/homework/lesson_4/level_6middle/task_17/Stock.java
@@ -6,6 +6,28 @@ class Stock {
     int currentPrice;
     int minimalPrice;
     int maximalPrice;
+    int newPrice;
+
+    Stock (String nameOfCompany,int currentPrice){
+        this.nameOfCompany = nameOfCompany;
+        this.currentPrice = currentPrice;
+    }
+
+    int getPriceInformation(){
+        return currentPrice;
+    }
+
+    int updatePrice (int newPrice ){
+        this.currentPrice = newPrice;
+        return currentPrice;
+    }
+
+
+    void info (){
+        System.out.println("Market price stock of company " + nameOfCompany + " " + getPriceInformation());
+    }
+
+
 
 
 
--- src/main/java/lv/javaguru/student_ivan_k/homework/lesson_4/level_6middle/task_17/StockDemo.java
+++ src/main/java/lv/javaguru/student_ivan_k/homework/lesson_4/level_6middle/task_17/StockDemo.java
@@ -3,7 +3,13 @@ package main.java.lv.javaguru.student_ivan_k.homework.lesson_4.level_6middle.tas
 class StockDemo {
 
     public static void main(String[] args) {
+        Stock google = new Stock("GOOGLE",10);
+        int priceInformation = google.getPriceInformation();
+        String nameOfCompany = google.nameOfCompany;
+        System.out.println("Market price stock of company "+ nameOfCompany + " " + priceInformation);
+        google.info();
+        google.updatePrice(11);
+        google.info();
 
-        public Stock()
     }
 }
