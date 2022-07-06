5eb4ec61fa360801bf8625efb3cf94f0994eada4
Miroslavs Fjodorovs <106261828+PecnayHeP@users.noreply.github.com>
2022-07-06 17:55:44 +0300

    Lesson 3: Homework

---
+++ src/main/java/lv/javaguru/student_miroslavs_fjodorovs/lesson_3/level_7/CatDemo.java
@@ -0,0 +1,18 @@
+package main.java.lv.javaguru.student_miroslavs_fjodorovs.lesson_3.level_7;
+
+class CatDemo {
+
+    public static void main(String[] args) {
+
+        Cat arya = new Cat("Arya", 3, true );
+
+        System.out.println("Name : " + arya.name);
+        System.out.println("Age : " + arya.age);
+        System.out.println("Is hungry : " + arya.isHungry);
+
+        arya.meow();
+        arya.eat();
+        System.out.println("Is not hungry : " + arya.isHungry);
+        arya.sleep();
+    }
+}
