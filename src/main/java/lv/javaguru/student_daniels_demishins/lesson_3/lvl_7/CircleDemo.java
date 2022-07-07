68d8ed716d3677f6f2f1ffaf1023800f47a8dbab
Wanxezar <zenadik@inbox.lv>
2022-07-07 12:57:14 +0300

    DD: HwLesson4

--- src/main/java/lv/javaguru/student_daniels_demishins/lesson_3/lvl_7/CircleDemo.java
+++ src/main/java/lv/javaguru/student_daniels_demishins/lesson_3/lvl_7/CircleDemo.java
@@ -4,10 +4,6 @@ public class CircleDemo {
     public static void main(String[] args) {
         Circle circle = new Circle(7.5, 3.14159, 2);
         double calculateArea = circle.getRadius()* circle.getPi()* circle.getDva();
-        System.out.println(calculateArea);
+        System.out.println("Area radius is a " +calculateArea);
     }
 }
-
-
-
-
