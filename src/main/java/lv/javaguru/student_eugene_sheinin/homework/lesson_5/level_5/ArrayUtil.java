d0ab90c9506befde648c2e03e3d4d33fe6bc59f3
Eugene Sheinin <favor2684@gmail.com>
2022-07-07 14:49:47 +0300

    Lesson 5 homework from student Eugene Sheinin

---
+++ src/main/java/lv/javaguru/student_eugene_sheinin/homework/lesson_5/level_5/ArrayUtil.java
@@ -0,0 +1,9 @@
+package main.java.lv.javaguru.student_eugene_sheinin.homework.lesson_5.level_5;
+
+public class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+
+        return new int[arrayLength];
+    }
+}
---
+++ src/main/java/lv/javaguru/teacher/lesson_5_arrays_for_loop/homework/level_5_middle/ArrayUtilTest.java
@@ -0,0 +1,15 @@
+package main.java.lv.javaguru.teacher.lesson_5_arrays_for_loop.homework.level_5_middle;
+
+public class ArrayUtilTest {
+
+    public static void main(String[] args) {
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+    }
+
+    public void shouldCreateArray() {
+
+    }
+
+
+}
