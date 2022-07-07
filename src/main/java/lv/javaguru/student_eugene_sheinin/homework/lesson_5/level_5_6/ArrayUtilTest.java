f1a00ecdf43e16f4c699050a6b351ac09101c081
Eugene Sheinin <favor2684@gmail.com>
2022-07-07 16:14:41 +0300

    Lesson 5 homework from student Eugene Sheinin

---
+++ src/main/java/lv/javaguru/student_eugene_sheinin/homework/lesson_5/level_5_6/ArrayUtilTest.java
@@ -0,0 +1,41 @@
+package main.java.lv.javaguru.student_eugene_sheinin.homework.lesson_5.level_5_6;
+
+public class ArrayUtilTest {
+    ArrayUtil arrayUtil = new ArrayUtil();
+
+    public static void main(String[] args) {
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+        test.shouldFindMaxNumber();
+        test.shouldFindMinNumber();
+    }
+
+    public void shouldCreateArray() {
+        int expectedResult = 5;
+        int actualResult = (this.arrayUtil.createArray(5)).length;
+        check(expectedResult, actualResult, "shouldCreateArray test");
+    }
+
+    public void shouldFindMaxNumber() {
+        int[] array = {4 , 8, 2, 6};
+        int expectedResult = 8;
+        int actualResult = this.arrayUtil.findMaxNumber(array);
+        check(expectedResult, actualResult, "shouldFindMaxNumber test");
+    }
+
+    public void shouldFindMinNumber() {
+        int[] array = {4 , 8, 3, 6};
+        int expectedResult = 3;
+        int actualResult = this.arrayUtil.findMinNumber(array);
+        check(expectedResult, actualResult, "shouldFindMinNumber test");
+    }
+
+    private void check(int expectedResult, int actualResult, String testName) {
+        if (expectedResult == actualResult) {
+            System.out.println(testName + " has passed.");
+        } else {
+            System.out.println(testName + " has failed!");
+            System.out.println("Expected: " + expectedResult + " but actual is: " + actualResult);
+        }
+    }
+}
