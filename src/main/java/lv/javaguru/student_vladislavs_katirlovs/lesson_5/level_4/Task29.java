0b63777815d2054044162c70303f08cc46a6fe2a
lFabrio <azureon94@gmail.com>
2022-07-12 19:33:09 +0300

    homework lesson 5

--- src/main/java/lv/javaguru/student_vladislavs_katirlovs/lesson_5/level_4/Task29.java
+++ src/main/java/lv/javaguru/student_vladislavs_katirlovs/lesson_5/level_4/Task29.java
@@ -3,21 +3,22 @@ package main.java.lv.javaguru.student_vladislavs_katirlovs.lesson_5.level_4;
 import java.util.Arrays;
 import java.util.Random;
 
-class Task28 {
+class Task29 {
 
     public static void main(String[] args) {
 
         Random random = new Random();
 
-        int[] array = new int[8];
+        int[] array = new int[5];
+        System.out.println("Even numbers in array are: ");
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(100);
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
         }
 
+        System.out.println("");
         System.out.println(Arrays.toString(array));
-        String minArray = String.valueOf(Arrays.stream(array).min());
-        minArray = minArray.replaceAll("[^0-9]", "");
-        System.out.println("Min number in array is " + minArray);
-
     }
 }
