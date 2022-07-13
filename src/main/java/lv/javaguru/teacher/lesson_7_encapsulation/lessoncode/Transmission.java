960889733aa41aea81e6a205f0ec562fd9cfe162
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2022-07-13 20:56:27 +0300

    Lesson 7: Encapsulation, Equals, ToString

---
+++ src/main/java/lv/javaguru/teacher/lesson_7_encapsulation/lessoncode/Transmission.java
@@ -0,0 +1,22 @@
+package main.java.lv.javaguru.teacher.lesson_7_encapsulation.lessoncode;
+
+import java.util.Objects;
+
+public class Transmission {
+
+    private boolean isAutomatic;
+    private String name;
+
+    public Transmission(boolean isAutomatic, String name) {
+        this.isAutomatic = isAutomatic;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transmission that = (Transmission) o;
+        return isAutomatic == that.isAutomatic && Objects.equals(name, that.name);
+    }
+}
