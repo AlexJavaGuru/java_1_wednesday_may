8d469cc676a036fe53948bda56b721c33fbf0dae
lFabrio <azureon94@gmail.com>
2022-07-16 01:58:33 +0300

    homework lesson 5

--- src/main/java/lv/javaguru/student_vladislavs_katirlovs/lesson_5/level_4/Task27.java
+++ src/main/java/lv/javaguru/student_vladislavs_katirlovs/lesson_5/level_4/Task27.java
@@ -1,22 +1,17 @@
 package main.java.lv.javaguru.student_vladislavs_katirlovs.lesson_5.level_4;
 
-import java.util.Arrays;
-import java.util.Random;
+import main.java.lv.javaguru.student_vladislavs_katirlovs.lesson_5.level_5_and_6.ArrayUtil;
 
 class Task27 {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
+        ArrayUtil arrayUtil = new ArrayUtil();
 
-        int[] array = new int[8];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(100);
-        }
-
-        System.out.println(Arrays.toString(array));
-        String maxArray = String.valueOf(Arrays.stream(array).max());
-        maxArray = maxArray.replaceAll("[^0-9]", "");
-        System.out.println("Max number in array is " + maxArray);
+        int[] array = arrayUtil.createArray(8);
+        arrayUtil.fillArrayWithRandomNumbers(array);
+        arrayUtil.printArrayToConsole(array);
+        int maxNumber = arrayUtil.findMaxNumber(array);
+        System.out.println(maxNumber + " is the biggest number");
     }
 }
--- src/main/java/lv/javaguru/student_vladislavs_katirlovs/lesson_5/level_4/Task29.java
+++ src/main/java/lv/javaguru/student_vladislavs_katirlovs/lesson_5/level_4/Task29.java
@@ -1,24 +1,21 @@
 package main.java.lv.javaguru.student_vladislavs_katirlovs.lesson_5.level_4;
 
-import java.util.Arrays;
-import java.util.Random;
+import main.java.lv.javaguru.student_vladislavs_katirlovs.lesson_5.level_5_and_6.ArrayUtil;
 
 class Task29 {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
+        ArrayUtil arrayUtil = new ArrayUtil();
 
-        int[] array = new int[5];
+        int[] array = arrayUtil.createArray(5);
+        arrayUtil.fillArrayWithRandomNumbers(array);
+        arrayUtil.printArrayToConsole(array);
         System.out.println("Even numbers in array are: ");
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(100);
             if (array[i] % 2 == 0) {
                 System.out.print(array[i] + " ");
             }
         }
-
-        System.out.println("");
-        System.out.println(Arrays.toString(array));
     }
 }
