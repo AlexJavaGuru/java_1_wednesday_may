cfedb6966543dc3c187efa7ce002bfb08b072297
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-07-18 15:38:34 +0300

    Lesson6: new tests

--- src/main/java/lv/javaguru/student_artjomsb_homework/homework6/level_6_middle/tictactoe/MariannaAITest.java
+++ src/main/java/lv/javaguru/student_artjomsb_homework/homework6/level_6_middle/tictactoe/MariannaAITest.java
@@ -4,15 +4,54 @@ import java.util.Arrays;
 
 class MariannaAITest {
     public static void main(String[] args) {
+        checkMoveInBestCorner();
         testCheckIfNextMoveWillMakeTwoPositionForWin();
         testCheckIfNextMoveWillMakeTwoPositionForWinTwo();
         testCheckIfNextMoveWillNotFindTwoPositionForWinTwo();
         checkNumberOfWins();
+        checkNumberOfWinsTwo();
         testBotMakeMove();
         testCheckMoveInRandomCorner();
         testRandomMove();
         checkThatNextMoveIsGetWin();
         checkThatNextMoveDontGetWin();
+        checkNotMoveInBestCorner();
+        testEmptyField();
+        testNotEmptyField();
+    }
+    static void testEmptyField(){
+        MariannaAI testAI = new MariannaAI(1, 0, "0", false);
+        String[][] testField = {{"-", "-", "-" },
+                {"-", "-", "-" },
+                {"-", "-", "-" }};
+        boolean result = testAI.isFieldEmpty(testField);
+        check(result, "Test check that field is empty");
+    }
+    static void testNotEmptyField(){
+        MariannaAI testAI = new MariannaAI(1, 0, "0", false);
+        String[][] testField = {{"-", "-", "-" },
+                {"-", "-", "-" },
+                {"-", "-", "0" }};
+        boolean result = testAI.isFieldEmpty(testField);
+        check(!result, "Test check that field is not empty");
+    }
+    static void checkMoveInBestCorner(){
+        MariannaAI testAI = new MariannaAI(1, 0, "0", false);
+        String[][] testField = {{"-", "X", "-" },
+                                {"-", "0", "-" },
+                                {"-", "0", "X" }};
+        boolean expectedResult = testAI.moveToBestCorner(testField,"X");
+        testAI.makeMove(testField);
+        check(expectedResult && testField[2][0].equals("0") && testField[0][0].equals("-") && testField[0][2].equals("-"),"Test check that next move will be in more winnable corner");
+    }
+    static void checkNotMoveInBestCorner(){
+        MariannaAI testAI = new MariannaAI(1, 1, "0", false);
+        String[][] testField = {{"-", "X", "-" },
+                                {"-", "-", "-" },
+                                {"-", "X", "-" }};
+        boolean expectedResult = testAI.moveToBestCorner(testField,"X");
+        testAI.makeMove(testField);
+        check(!expectedResult && testField[2][0].equals("-") && testField[0][0].equals("-") && testField[0][2].equals("-") && testField[2][2].equals("-") && testField[1][1].equals("0"),"Test check that next move not go in corner");
     }
     static void checkThatNextMoveIsGetWin(){
         MariannaAI testAI = new MariannaAI(0, 0, "0", false);
@@ -44,12 +83,21 @@ static void testRandomMove(){
     check(Arrays.deepEquals(testField,expectedField), "Check that random move didn't go to occupied cell");
 }
     static void checkNumberOfWins() {
-        MariannaAI testAI = new MariannaAI(0, 0, "0", false);
+        MariannaAI testAI = new MariannaAI(0, 0, "X", false);
         String[][] testField = {{"X", "-", "X" },
-                {"-", "0", "-" },
-                {"0", "-", "X" }};
+                                {"-", "0", "-" },
+                                {"X", "-", "X" }};
         int result = testAI.howManyWinsIsPossible(testField, "X");
-        int expectedResult = 2;
+        int expectedResult = 4;
+        check(result == expectedResult, "Check number of wins");
+    }
+    static void checkNumberOfWinsTwo() {
+        MariannaAI testAI = new MariannaAI(0, 0, "X", false);
+        String[][] testField = {{"X", "-", "0" },
+                                {"0", "0", "-" },
+                                {"-", "0", "X" }};
+        int result = testAI.howManyWinsIsPossible(testField, "X");
+        int expectedResult = 0;
         check(result == expectedResult, "Check number of wins");
     }
 
