842ad4572f7f76d1b8288198ecf5480bba399f25
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-07-21 07:21:30 +0300

    Lesson7: level 7 senior half of task

--- src/main/java/lv/javaguru/student_artjomsb_homework/lesson_7/homework/level_7_senior/UserEntityRepository.java
+++ src/main/java/lv/javaguru/student_artjomsb_homework/lesson_7/homework/level_7_senior/UserEntityRepository.java
@@ -1,22 +1,39 @@
 package main.java.lv.javaguru.student_artjomsb_homework.lesson_7.homework.level_7_senior;
 
+import java.util.Arrays;
+
 class UserEntityRepository {
-    private int id;
-    private UserEntity[] allUsers;
+    private int id = 0;
+
+    private UserEntity[] allUsers = new UserEntity[1];
 
     void saveUser(UserEntity newUser) {
         if (isPersonalIdUnique(newUser)) {
+            findFreeID();
+            newUser.setId(id);
             allUsers[id] = newUser;
-            id++;
         } else {
             System.out.println("Error, user with the same personal-id registered");
         }
     }
 
-    UserEntity getUserName(String name) {
+    private void findFreeID() {
         for (int i = 0; i < allUsers.length; i++) {
-            if (name.equals(allUsers[i].getUserName())) {
-                return allUsers[i];
+            if (allUsers[i] == null) {
+                id = i;
+                break;
+            }
+        }
+        if (allUsers[id] != null) {
+            allUsers = Arrays.copyOf(allUsers, allUsers.length + 1);
+            id = allUsers.length - 1;
+        }
+    }
+
+    UserEntity getUserByName(String name) {
+        for (UserEntity allUser : allUsers) {
+            if (allUser != null && name.equals(allUser.getUserName())) {
+                return allUser;
             }
         }
         return null;
@@ -24,13 +41,22 @@ class UserEntityRepository {
 
 
     boolean isPersonalIdUnique(UserEntity user) {
-        int userId = user.getId();
+        String userId = user.getPersonalId();
         for (UserEntity allUser : allUsers) {
-            if (userId == allUser.getId()) {
+            if (allUser != null && userId.equals(allUser.getPersonalId())) {
                 return false;
             }
         }
         return true;
+    }
+
+    void deleteUser(String personalId) {
+        for (int i = 0; i < allUsers.length; i++) {
+            if (allUsers[i] != null && allUsers[i].getPersonalId().equals(personalId)) {
+                allUsers[i] = null;
+                break;
+            }
+        }
     }
 
     UserEntity getUserWithId(int id) {
