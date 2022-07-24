a4da8568cd556f2cdef6814ad616e8aac5c0d572
Andrei <andrey19v@icloud.com>
2022-07-24 23:41:38 +0300

    Lesson 5: (Java) Andrei Dementiev

---
+++ src/main/java/lv/javaguru/student_andrei_de/Homework/lesson5/task17.java
@@ -0,0 +1,14 @@
+package main.java.lv.javaguru.student_andrei_de.Homework.lesson5;
+
+public class task17 {
+    class ForLoopDefinition2 {
+
+        public static void main(String[] args) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
