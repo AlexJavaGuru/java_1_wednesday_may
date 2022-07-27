568a5dcb98239511a6a6993e5c0a48060123c9b1
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2022-07-27 21:13:03 +0300

    Lesson 9: Interfaces

---
+++ src/main/java/lv/javaguru/teacher/lesson_9_interfaces/lessoncode/boxexample/Demo.java
@@ -0,0 +1,17 @@
+package main.java.lv.javaguru.teacher.lesson_9_interfaces.lessoncode.boxexample;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        BoxService boxService = new BoxServiceNew();
+        WarehouseService warehouseService = new WarehouseService(boxService);
+
+        Box box = new Box();
+        Item item = new Item();
+
+        warehouseService.putSomePackageIntoTheBox(box, item);
+        warehouseService.sortTheBox(box);
+        warehouseService.closeTheBox(box);
+    }
+}
