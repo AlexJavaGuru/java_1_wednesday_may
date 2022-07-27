bd3d5630c8dfe454039034c944c8ce12adc81637
VladKuli <goldydefault@gmail.com>
2022-07-28 01:59:57 +0300

    Vladislav Kulikov Lesson 8: All tasks

---
+++ src/main/java/lv/javaguru/student_vladislav_kulikov/Lesson_8/Task_32/MathOperation.java
@@ -0,0 +1,7 @@
+package main.java.lv.javaguru.student_vladislav_kulikov.Lesson_8.Task_32;
+
+abstract class MathOperation {
+
+	public abstract double calculate();
+
+}
---
+++ src/main/java/lv/javaguru/student_vladislav_kulikov/Lesson_8/Task_32/Multiply.java
@@ -0,0 +1,15 @@
+package main.java.lv.javaguru.student_vladislav_kulikov.Lesson_8.Task_32;
+
+class Multiply extends TwoArgumentMathOperation {
+
+	public Multiply(MathOperation leftArgument,
+                    MathOperation rightArgument) {
+		super(leftArgument, rightArgument);
+	}
+
+	@Override
+	public double calculate() {
+		return calculateLeftSide() * calculateRightSide();
+	}
+}
+
