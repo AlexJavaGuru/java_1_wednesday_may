eb68313188e9c978d30a2fe742d0b5bc67504a3f
Somalo99 <rpismenny94@gmail.com>
2022-08-03 00:54:16 +0300

    Full Lesson 7

---
+++ src/main/java/lv/javaguru/student_rodions_pismennijs/lesson_7/level_5/PowerCalculatorTest.java
@@ -0,0 +1,30 @@
+package main.java.lv.javaguru.student_rodions_pismennijs.lesson_7.level_5;
+
+class PowerCalculatorTest {
+
+    public static void main(String[] args) {
+        PowerCalculatorTest test = new PowerCalculatorTest();
+        test.powerTest1();
+        test.powerTest2();
+    }
+
+    void check(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " - OK");
+        } else {
+            System.out.println(testName + " - FAIL");
+        }
+    }
+
+    PowerCalculator powerCalculator = new PowerCalculator();
+
+    void powerTest1() {
+        int result = powerCalculator.power(3, 3);
+        check(result == 27, "Power 3^3 test");
+    }
+
+    void powerTest2() {
+        int result = powerCalculator.power(8, 2);
+        check(result == 64, "Power 8^2 test");
+    }
+}
