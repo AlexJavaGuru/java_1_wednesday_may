467bafc252818cdbcfed818bbb1d0222fc75e948
Vadim_V <dogfather@inbox.lv>
2022-08-06 16:53:35 +0300

    Vadims_Vilhovojs_lesson_6_1

--- src/main/java/lv/javaguru/student_vadims_vilhovojs/lesson_6/NumberUtilsTest.java
+++ src/main/java/lv/javaguru/student_vadims_vilhovojs/lesson_6/NumberUtilsTest.java
@@ -1,4 +1,23 @@
 package main.java.lv.javaguru.student_vadims_vilhovojs.lesson_6;
 
 class NumberUtilsTest {
-}
+    public static void main(String[] args) {
+        NumberUtilsTest test = new NumberUtilsTest();
+        test.isEven();
+    }
+    public void isEven(){
+        NumberUtils numberUtils = new NumberUtils();
+        int number = 8;
+        boolean expectedResult = true;
+        boolean actualResult = numberUtils.isEven(number);
+        if (actualResult == true) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+
+            }
+        }
+
+
+    }
+
