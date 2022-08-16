d90cab94ac7bd5b9297d471905435c789c80b319
Eugene Sheinin <favor2684@gmail.com>
2022-08-16 17:49:54 +0300

    Lesson 9 homework

--- src/main/java/lv/javaguru/student_eugene_sheinin/homework/lesson_9/level_5_6/BookReaderTest.java
+++ src/main/java/lv/javaguru/student_eugene_sheinin/homework/lesson_9/level_5_6/BookReaderTest.java
@@ -1,4 +1,4 @@
-package main.java.lv.javaguru.student_eugene_sheinin.homework.lesson_9.level_5;
+package main.java.lv.javaguru.student_eugene_sheinin.homework.lesson_9.level_5_6;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +15,97 @@ public class BookReaderTest {
         testResultListSizeFindBooksByAuthor();
         testFindBooksByAuthor();
         testFindBooksByAuthorNameOnly();
+        testFindBooksContainsTitleOnly();
+        testFindBooksByTitle();
+        testMarkReadBooksBooks();
+        testMarkUnreadBooksBooks();
+    }
+
+    private static void testMarkReadBooksBooks() {
+        BookReader bookReader = new BookReaderImpl();
+        Book book1 = new Book("Alex Djuma", "Tri mushketjora");
+        Book book2 = new Book("Tugenev", "Mu-mu");
+        Book book3 = new Book("Pushkin", "Skazka o carje");
+        Book book4 = new Book("Alex Djuma", "Tri mushketjora 20 let spustja");
+        bookReader.addBook(book1);
+        bookReader.addBook(book2);
+        bookReader.addBook(book3);
+        bookReader.addBook(book4);
+        bookReader.markAsReadBook(book2);
+        bookReader.markAsReadBook(book3);
+
+        String[] expectedTitles = new String[2];
+        expectedTitles[0] = "Mu-mu";
+        expectedTitles[1] = "Skazka o carje";
+        Book[] resultList = bookReader.getReadedBookList().toArray(new Book[2]);
+        for (int i = 0; i < resultList.length; i++) {
+            check(
+                    expectedTitles[i].equals(resultList[i].getTitle()),
+                    "testMarkReadBooksBooks: " + i
+            );
+        }
+    }
+
+    private static void testMarkUnreadBooksBooks() {
+        BookReader bookReader = new BookReaderImpl();
+        Book book1 = new Book("Alex Djuma", "Tri mushketjora");
+        Book book2 = new Book("Tugenev", "Mu-mu");
+        Book book3 = new Book("Pushkin", "Skazka o carje");
+        Book book4 = new Book("Alex Djuma", "Tri mushketjora 20 let spustja");
+        bookReader.addBook(book1);
+        bookReader.addBook(book2);
+        bookReader.addBook(book3);
+        bookReader.addBook(book4);
+        bookReader.markAsReadBook(book2);
+        bookReader.markAsReadBook(book3);
+        bookReader.markAsReadBook(book4);
+        bookReader.markAsUnreadBook(book3);
+
+        String[] expectedTitles = new String[2];
+        expectedTitles[0] = "Mu-mu";
+        expectedTitles[1] = "Tri mushketjora 20 let spustja";
+        Book[] resultList = bookReader.getReadedBookList().toArray(new Book[2]);
+        for (int i = 0; i < resultList.length; i++) {
+            check(
+                    expectedTitles[i].equals(resultList[i].getTitle()),
+                    "testMarkReadBooksBooks: " + i
+            );
+        }
+    }
+
+    private static void testFindBooksContainsTitleOnly() {
+        BookReader bookReader = new BookReaderImpl();
+        bookReader.addBook(new Book("Alex Djuma", "Tri mushketjora"));
+        bookReader.addBook(new Book("Tugenev", "Mu-mu"));
+        bookReader.addBook(new Book("Pushkin", "Skazka o carje"));
+        bookReader.addBook(new Book("Alex Djuma", "Tri mushketjora 20 let spustja"));
+        String[] expectedTitles = new String[2];
+        expectedTitles[0] = "Tri mushketjora";
+        expectedTitles[1] = "Tri mushketjora 20 let spustja";
+        Book[] resultList = bookReader.findBooksByTitle("Tri").toArray(new Book[2]);
+        for (int i = 0; i < resultList.length; i++) {
+            check(
+                    expectedTitles[i].equals(resultList[i].getTitle()),
+                    "testFindBooksContainsTitleOnly: " + i
+            );
+        }
+    }
+
+    private static void testFindBooksByTitle() {
+        BookReader bookReader = new BookReaderImpl();
+        bookReader.addBook(new Book("Djuma", "Tri mushketjora"));
+        bookReader.addBook(new Book("Tugenev", "Mu-mu"));
+        bookReader.addBook(new Book("Pushkin", "Skazka o carje"));
+        bookReader.addBook(new Book("Djuma", "20 let spustja"));
+        String[] expectedTitles = new String[2];
+        expectedTitles[0] = "Tri mushketjora";
+        Book[] resultList = bookReader.findBooksByTitle("Tri mushketjora").toArray(new Book[1]);
+        for (int i = 0; i < resultList.length; i++) {
+            check(
+                    expectedTitles[i].equals(resultList[i].getTitle()),
+                    "testFindBooksByTitle: " + i
+            );
+        }
     }
 
     private static void testFindBooksByAuthorNameOnly() {
