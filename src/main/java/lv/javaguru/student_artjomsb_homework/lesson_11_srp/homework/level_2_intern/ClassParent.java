81f91f684d84998dbcd1353a30dbdfdc41b709eb
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-08-23 10:37:53 +0300

    Lesson11: level2

---
+++ src/main/java/lv/javaguru/student_artjomsb_homework/lesson_11_srp/homework/level_2_intern/ClassParent.java
@@ -0,0 +1,7 @@
+package main.java.lv.javaguru.student_artjomsb_homework.lesson_11_srp.homework.level_2_intern;
+
+class ClassParent {
+    protected ClassParent() {
+        System.out.println("Parent");
+    }
+}
---
+++ src/main/java/lv/javaguru/student_artjomsb_homework/lesson_11_srp/homework/level_2_intern/ClassY.java
@@ -0,0 +1,8 @@
+package main.java.lv.javaguru.student_artjomsb_homework.lesson_11_srp.homework.level_2_intern;
+
+class ClassY extends ClassX {
+    public ClassY(int i) {
+        super(i);
+        System.out.println(2);
+    }
+}
