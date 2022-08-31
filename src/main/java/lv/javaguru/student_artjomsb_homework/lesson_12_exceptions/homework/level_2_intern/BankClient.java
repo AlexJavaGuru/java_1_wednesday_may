57c360a272167531b6ca149db03ec60b845413b6
ArtjomsBogatirjovs <ar.bogatirjovs@gmail.com>
2022-08-31 10:51:28 +0300

    Lesson12: level 2

---
+++ src/main/java/lv/javaguru/student_artjomsb_homework/lesson_12_exceptions/homework/level_2_intern/BankClient.java
@@ -0,0 +1,20 @@
+package lv.javaguru.student_artjomsb_homework.lesson_12_exceptions.homework.level_2_intern;
+
+class BankClient {
+
+    private String uid;      // уникальный идентификатор клиента
+    private String fullName; // полное имя клиента
+
+    public BankClient(String uid, String fullName) {
+        this.uid = uid;
+        this.fullName = fullName;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
---
+++ src/main/java/lv/javaguru/student_artjomsb_homework/lesson_12_exceptions/homework/level_2_intern/Role.java
@@ -0,0 +1,5 @@
+package lv.javaguru.student_artjomsb_homework.lesson_12_exceptions.homework.level_2_intern;
+
+enum Role {
+    CAN_SEARCH_CLIENTS
+}
